f93b7c60860feac185c6533f1a4a5c118ecf90cf
Paula Micheli <paula.michele@grupobrisanet.com.br>
2023-08-21 10:28:03 -0300

    :tada: initial commit

---
+++ fundamentos/primeiro/primeiro.go
@@ -0,0 +1,14 @@
+package main
+
+// Progamas executáveis iniciam pelo pacote main
+
+/*
+Os códigos em Go são organizados em pacotes
+e para usá-los é necessário declarar um ou vários imports
+*/
+
+import "fmt"
+
+func main() {
+	fmt.Println("Hello world!")
+}
